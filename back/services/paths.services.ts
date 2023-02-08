682e257a2dc0326c016591293545309361dc2a27
dpletzke <dpletzke@gmail.com>
2023-02-08 14:26:22 -0500

    work on use promises in services, access db correctly

--- back/services/paths.services.ts
+++ back/services/paths.services.ts
@@ -1,14 +1,8 @@
 //this is the service for the creation of a path with mongoDB
 import { Path } from "../types";
-const pathServices = (app: any, db: any) => {
-  app.post("/path", (req: any, res: any) => {
-    const path: Path = {
-      name: req.body.name,
-      description: req.body.description,
-      photos: req.body.photos,
-    };
+const pathServices = (app: any, db: ) => {
 
-    db.collection("paths")
+    const makePath = (path: Path) => db.collection("paths")
       .insert(path)
       .then((result: any) => {
         res.send(result.ops[0]);
@@ -16,27 +10,33 @@ const pathServices = (app: any, db: any) => {
       .catch((err: any) => {
         res.send({ error: "An error has occurred" });
       });
-  });
-  app.get("/path", (req: any, res: any) => {
-    db.collection("paths")
-      .find({})
-      .toArray((err: any, result: any) => {
-        if (err) {
-          res.send({ error: "An error has occurred" });
-        } else {
-          res.send(result);
-        }
-      });
-  });
-  app.get("/path/:id", (req: any, res: any) => {
-    db.collection("paths")
-      .find({ _id: req.params.id })
-      .toArray((err: any, result: any) => {
-        if (err) {
-          res.send({ error: "An error has occurred" });
-        } else {
-          res.send(result);
-        }
-      });
-  });
-};
+
+      return {
+        makePath
+      }
+  };
+  // app.get("/path", (req: any, res: any) => {
+  //   db.collection("paths")
+  //     .find({})
+  //     .toArray((err: any, result: any) => {
+  //       if (err) {
+  //         res.send({ error: "An error has occurred" });
+  //       } else {
+  //         res.send(result);
+  //       }
+  //     });
+  // });
+  // app.get("/path/:id", (req: any, res: any) => {
+  //   db.collection("paths")
+  //     .find({ _id: req.params.id })
+  //     .toArray((err: any, result: any) => {
+  //       if (err) {
+  //         res.send({ error: "An error has occurred" });
+  //       } else {
+  //         res.send(result);
+  //       }
+  //     });
+  // });
+// };
+
+module.exports = pathServices;
