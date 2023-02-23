f1eb419f0549e827cbc7a59f37c798fcb0429a90
dpletzke <dpletzke@gmail.com>
2023-02-23 03:09:53 -0500

    add hook to throw error if db connection is down

--- back/src/plugins/db.ts
+++ back/src/plugins/db.ts
@@ -9,6 +9,14 @@ const plugin: FastifyPluginCallback = async (server, opts, next) => {
     forceClose: true,
     url: env.MONGO_URI,
   });
+
+  // if the database connection doesn't exist, throw an error
+  server.addHook("onRequest", async (req, reply) => {
+    if (!server.mongo.db) {
+      throw new Error("No database connection");
+    }
+  });
+
   next();
 };
 
