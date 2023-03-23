f12d4b0c7c8f03143ad0580ac7eb4640f8710f82
dpletzke <dpletzke@gmail.com>
2023-03-23 18:32:38 -0500

    gen user crud routes and test auth

--- back/routes/v1/users.v1.routes.js
+++ back/routes/v1/users.v1.routes.js
@@ -1,7 +1,75 @@
 "use strict";
 
+const passport = require("@fastify/passport");
+
 module.exports = async function (fastify, opts) {
-  fastify.get("/users", async function (request, reply) {
-    return { root: true };
+  // fastify.get(
+  //   "/user",
+  //   {
+  //     preValidation: [fastify.auth()],
+  //   },
+  //   async function (request, reply) {
+  //     const users = await fastify.mongo.db
+  //       .collection("users")
+  //       .find({})
+  //       .toArray();
+  //     return { data: users };
+  //   }
+  // );
+
+  fastify.get(
+    "/user",
+    {
+      preValidation: [
+        passport.authenticate(
+          "jwt",
+          { authInfo: false },
+          async (request, reply, err, user, info, status) => {
+            console.log({ err, user, info, status });
+            if (err || info || !user) {
+              return Promise.reject(new Error("Please authenticate"));
+            }
+          }
+        ),
+      ],
+    },
+    async function (request, reply) {
+      const users = await fastify.mongo.db
+        .collection("users")
+        .find({})
+        .toArray();
+      return { data: users };
+    }
+  );
+
+  fastify.post("/user", async function (request, reply) {
+    const user = await fastify.mongo.db
+      .collection("users")
+      .insertOne(request.body);
+    return { data: user };
+  });
+
+  fastify.get("/user/:id", async function (request, reply) {
+    const user = await fastify.mongo.db
+      .collection("users")
+      .findOne({ _id: new fastify.mongo.ObjectId(request.params.id) });
+    return { data: user };
+  });
+
+  fastify.put("/user/:id", async function (request, reply) {
+    const user = await fastify.mongo.db
+      .collection("users")
+      .updateOne(
+        { _id: new fastify.mongo.ObjectId(request.params.id) },
+        { $set: request.body }
+      );
+    return { data: user };
+  });
+
+  fastify.delete("/user/:id", async function (request, reply) {
+    const user = await fastify.mongo.db
+      .collection("users")
+      .deleteOne({ _id: new fastify.mongo.ObjectId(request.params.id) });
+    return { data: user };
   });
 };
