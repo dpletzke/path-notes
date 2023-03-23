138c3244585e3f11b8781cb3acd98f2f64061387
dpletzke <dpletzke@gmail.com>
2023-03-23 18:32:09 -0500

    use fastify plugin for mongo

--- back/plugins/mongodb.plugin.js
+++ back/plugins/mongodb.plugin.js
@@ -3,11 +3,9 @@ const fp = require("fastify-plugin");
 require("dotenv").config();
 
 module.exports = fp(async function (fastify, opts) {
-  console.log(process.env.MONGO_URI);
-  const client = await mongodb.MongoClient.connect(
-    process.env.MONGO_URI,
-    opts.options
-  );
-  const db = client.db(opts.dbName);
-  fastify.decorate("mongo", db);
+  fastify.register(require("@fastify/mongodb"), {
+    url: process.env.MONGO_URI,
+    database: "path-notes",
+    forceClose: true,
+  });
 });
